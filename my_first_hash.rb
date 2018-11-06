75b1bcec3a3e07c3d6313218681113273234d2bc
sensible-proc-7557 <vchan313@gmail.com>
2018-11-06 23:22:38 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,6 +23,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest['oil paintings']
 
 end
 
@@ -33,7 +34,8 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest['muskets'] => 2
+  
 
 
   # add 4 gun powder to the shipping_manifest hash below
